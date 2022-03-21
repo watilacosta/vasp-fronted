f7367ed964240d4469c4f9fc7955f01e159639fc
Watila Costa <watila.freire@repassa.com.br>
2022-03-21 00:26:10 -0300

    added i18n

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App)
+createApp(App).use(i18n)
   .use(router)
   .use(store)
   .mount('#app')
 
 import "bootstrap/dist/js/bootstrap.js"
+import i18n from './i18n'
