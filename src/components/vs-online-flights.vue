1f552f09859d8bd7d5d009dcaf9a549c48283d42
Watila Costa <watila.freire@repassa.com.br>
2022-03-23 15:30:13 -0300

    feature links to our partners

--- src/components/vs-online-flights.vue
+++ src/components/vs-online-flights.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="onlin-flights">
+  <div class="online-flights">
     <h1 class="display-6 text-muted text-center my-4">{{ $t('home-page.titles.online-flights') }}</h1>
     <iframe src="https://uesva.com:3008/addons/iframe?uid=a0hhOElLYkxkbkdjNXowcXdxdCtudWV2RzNzRExBTmIxV0x0NUd3dWhhVT0="
       scrolling="no"
@@ -15,3 +15,8 @@ export default {
   name: 'online-flights'
 }
 </script>
+<style scoped>
+.online-flights {
+  margin-top: 4rem;
+}
+</style>
--- src/components/vs-partners.vue
+++ src/components/vs-partners.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="partners">
-    <h1 class="display-6 text-muted text-center mb-2">
+    <h1 class="display-6 text-muted text-center mb-4">
       {{ $t('partners') }}
     </h1>
 
-    <div class="d-flex justify-content-between">
-    
+    <div class="d-flex justify-content-evenly">
+      <a href="https://www.vatsim.net/" target="_blank">
+        <img class="img-partners" src="../assets/images/vatsim.png" />
+      </a>
+      <a href="https://www.voeclimb.com/welcome/" target="_blank">
+        <img class="img-partners climb" src="../assets/images/climb.png" />
+      </a>
+      <a href="https://www.ivao.aero/" target="_blank">
+        <img class="img-partners" src="../assets/images/ivao.svg" />
+      </a>
     </div>
   </div>
 </template>
@@ -15,3 +23,13 @@ export default {
   name: 'vs-partners'
 }
 </script>
+<style scoped>
+  .img-partners {
+    height: 80px;
+    width: 200px;
+  }
+  .img-partners.climb {
+    height: 60px;
+    margin-top: 1em;
+  }
+</style>
