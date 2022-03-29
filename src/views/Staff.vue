2a10e4370da2a8b544d819adea7317c0acd92517
Watila Costa <watila.freire@repassa.com.br>
2022-03-29 20:14:07 -0300

    staff profile

--- src/views/Staff.vue
+++ src/views/Staff.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="staff">
     <h1 class="display-6 text-muted text-center mb-4">{{ $t('page-staff.title') }}</h1>
-    <div class="table-responsive">
-      <table class="table table-bordered table-hover table-striped">
-        <thead class="table-dark">
-          <tr class="text-center">
-            <th scope="col">{{ $t('page-staff.table.th.name') }}</th>
-            <th scope="col">{{ $t('page-staff.table.th.callsign') }}</th>
-            <th scope="col">{{ $t('page-staff.table.th.title') }}</th>
-            <th scope="col">{{ $t('page-staff.table.th.email') }}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="text-center" v-for="(staff, id) in staffs" :key="id">
-            <td>{{ staff.name }}</td>
-            <td>{{ staff.callsign }}</td>
-            <td>{{ staff.title.toUpperCase() }}</td>
-            <td>{{ staff.staffMail }}</td>
-          </tr>
-        </tbody>
-      </table>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col" v-for="(staff, id) in staffs" :key="id">
+        <div class="card h-100">
+          <img :src="staff.img1" class="card-img-top" alt="imagem do staff" >
+          <div class="card-body">
+            <h5 class="card-title">{{ staff.callsign }} - {{ staff.name }}</h5>
+            <p class="card-text">{{ staff.title.toUpperCase() }}</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">{{ staff.staffMail }}</small>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
