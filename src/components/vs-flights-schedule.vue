396cd815856d6f61c29a679ac47e978825d2fd46
Watila Costa <watila.freire@repassa.com.br>
2022-03-30 20:08:40 -0300

    added flight schedule

---
+++ src/components/vs-flights-schedule.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flights-schedule">
+    <h1 class="display-6 text-muted text-center my-4">{{ $t('flights-schedule.title') }}</h1>
+    <iframe
+      src="https://uesva.com:3008/addons/iframe?uid=c2ZLQUJOdXZrV1M2Wk40M09BMmNiaEJRYTJpdHJwQlZsNFlkc1VHUW1URT0="
+      scrolling="no"
+      width="100%"
+      height="300"
+      frameborder="0"
+      name="PBYMzqTwZb1648681072">
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vs-flights-schedule',
+}
+</script>
+<style scoped>
+.flights-schedule {
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <vs-header></vs-header>
     <vs-partners></vs-partners>
+    <vs-flights-schedule></vs-flights-schedule>
     <vs-online-flights></vs-online-flights>
     <vs-airlines-rank></vs-airlines-rank>
     <vs-map-destinations></vs-map-destinations>
@@ -14,9 +15,17 @@ import VsHeader from '../components/vs-header.vue'
 import VsOnlineFlights from '../components/vs-online-flights.vue'
 import VsPartners from '../components/vs-partners.vue'
 import VsMapDestinations from '../components/vs-map-destinations.vue'
+import VsFlightsSchedule from '../components/vs-flights-schedule.vue'
 
 export default {
   name: 'Home',
-  components: {VsHeader, VsPartners, VsOnlineFlights, VsAirlinesRank, VsMapDestinations}
+  components: {
+    VsHeader,
+    VsPartners,
+    VsOnlineFlights,
+    VsAirlinesRank,
+    VsMapDestinations,
+    VsFlightsSchedule
+  }
 }
 </script>
